9d29131388c123c5668e31638895a95f4df585c0
mstarkhulk <mstarkhulk@163.com>
2018-11-15 22:41:39 +0800

    用户管理表格完成

---
+++ public/back/js/user.js
@@ -0,0 +1,24 @@
+$(function() {
+
+    // 一进入页面, 发送ajax请求, 获取数据, 进行页面动态渲染
+    $.ajax({
+      type: "get",
+      url: "/user/queryUser",
+      data: {
+        page: 1,
+        pageSize: 5
+      },
+      dataType: "json",
+      success: function( info ) {
+        console.log( info );
+        // 生成 htmlStr, 将来进行渲染
+        // 参数1: 模板id, 参数2: 数据对象
+        // 在模板中, 可以直接访问传进去对象中的所有属性
+        var htmlStr = template("tmp", info );
+  
+        $('tbody').html( htmlStr );
+      }
+    })
+  
+  
+  })
